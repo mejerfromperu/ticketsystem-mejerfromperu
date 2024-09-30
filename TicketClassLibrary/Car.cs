5d6de4523e5239e35fb89e11c5381f44073076a3
Christian Alexander Yábar Mejer <chm003@edu.zealand.dk>
2024-09-30 10:30:33 +0200

    first 2 task

---
+++ TicketClassLibrary/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketClassLibrary
+{
+    /// <summary>
+    /// Klasse car som indeholder 2 props licensplate og date
+    /// </summary>
+    public class Car
+    {
+
+        private string _licensPlate;
+        private DateTime _date;
+
+        public string LicensPlate
+        {
+            get { return _licensPlate; }
+            set { _licensPlate = value; }
+        }
+
+        public DateTime Date
+        { 
+            get { return _date; } 
+            set { _date = value; } 
+        }
+
+        /// <summary>
+        /// Metode til prisen som er fixed på 240
+        /// </summary>
+        /// <returns>prisen som er 240</returns>
+        public double Price()
+        {
+            return 240;
+        }
+        /// <summary>
+        /// public metode som retunere en string om hvad køretøj det er
+        /// </summary>
+        /// <returns>retunere "car"</returns>
+        public string Vehicle()
+        {
+            return "Car";
+        }
+
+
+
+    }
+}
---
+++ TicketClassLibraryTests/CarTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketClassLibrary.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        [TestMethod()]
+        public void PriceTest()
+        {
+            // Arange
+            Car car = new Car();
+
+            // ACt
+            double expectedPrice = 240;
+
+            // ASsert
+            Assert.AreEqual(expectedPrice, car.Price());
+            
+        }
+
+        [TestMethod()]
+        public void VehicleTest()
+        {
+            // Arange
+            Car car = new Car();
+
+            // ACt
+
+            // Assert
+            Assert.AreEqual("car", car.Vehicle().ToLower());
+        }
+    }
+}
