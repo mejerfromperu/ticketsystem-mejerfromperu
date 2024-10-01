90e75d61354f856ac16b4b2fe18aa2ec3e8501f9
mejerfromperu <christianmejer7@gmail.com>
2024-10-01 23:34:20 +0200

    repo added

--- RepositoryStorbeltbroen/Repository.cs
+++ RepositoryStorbeltbroen/Repository.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using StoreBaeltTicketLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TicketClassLibrary;
+
+
+
 
 namespace RepositoryStorbeltbroen
 {
-    public class Repository
+    public class Repository 
     {
 
-        //private static List<Vehicle> tickets = new List<Vehicle>();
+        //private static List<Vehicle> carTickets = new List<>();
+        //private static List<> mcTickets = new List<>();
 
 
+        
 
     }
 }
